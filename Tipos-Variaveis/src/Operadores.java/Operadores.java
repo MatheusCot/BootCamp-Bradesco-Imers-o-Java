5fd770f4ed81f0da4149cfc3d6ec15c037e630f1
Matheus Cota <bergensmatheus@gmail.com>
2025-04-01 21:41:15 -0300

    Operadores

---
+++ Tipos-Variaveis/src/Operadores.java/Operadores.java
@@ -0,0 +1,14 @@
+package Operadores.java;
+
+public class Operadores {
+    public static void main(String [] args) {
+        int numero = 5;
+        //x repeticao
+        numero = numero + 2;
+
+        System.out.println(numero);
+
+    }
+
+
+}
---
+++ Tipos-Variaveis/src/Operadores/Operadores.java
@@ -0,0 +1,18 @@
+package Operadores;
+
+public class Operadores {
+    public static void main(String [] args) {
+        int numero = 5;
+        //x repeticao
+        numero++;
+        //numero = numero +1;
+
+
+        System.out.println( ++ numero );
+       
+        System.out.println(numero);
+
+    }
+
+
+}
